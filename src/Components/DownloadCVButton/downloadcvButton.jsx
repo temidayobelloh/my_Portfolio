f0adf380a73dae1014638be74d153479d2912c87
temidayobelloh <temidayobelloh@gmail.com>
2024-06-13 13:52:56 +0100

    worked on the download cv button & inserted my cv URL

---
+++ src/Components/DownloadCVButton/downloadcvButton.jsx
@@ -0,0 +1,24 @@
+
+
+const DownloadcvButton = () => {
+    const cvFile ='https://drive.google.com/file/d/1ayg6tQtjlivKYNXGArkMFIfPawZ-RzO4/view?usp=sharing';
+    const fileName ="Temidayo Bello CV";
+    //const cvFile = `${process.env.PUBLIC_URL}/Temidayo Bello Hajarah.pdf`;
+    //const link = document.createElement('a');
+    //link.href = cvFile;
+    //link.download = 'TemidayoBelloCV'; //specify the name that i want the downloaded CV to have
+    //document.body.appendChild(link);
+    //link.click();
+    //document.body.removeChild(link);
+
+  return (
+    <div>
+    <a href={cvFile} download ={fileName} >
+    <button className='homePage-button' onClick={DownloadcvButton}>Download CV</button>
+    </a>
+      
+    </div>
+  )
+}
+
+export default DownloadcvButton;
--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -2,6 +2,7 @@ import './home.css';
 import About from '../About/about';
 import Skills from '../MySkills/skills';
 import Footer from '../Footer/footer';
+import DownloadcvButton from '../DownloadCVButton/downloadcvButton';
 
 
 const Home = () => {
@@ -12,7 +13,7 @@ const Home = () => {
     <p className='homePage-text-one'>Hello! I am <span className='text-name'>Bello TEMIDAYO </span></p>
     <p className='homePage-text-two'>I am a passionate Frontend Developer, who loves turning ideas into reality through code.<br></br>I specialize in creating visually appealing and functional websites using HTML, CSS, JavaScript, and React.  <br></br> I am actively searching for entry-level positions to utilize my skills and advance in my profession</p>
     <div className='button-wrapper'>
-    <button className='homePage-button'> Download CV </button>
+    <DownloadcvButton/>
     <button className='homePage-button'>View Projects</button>
     </div>
     </div>

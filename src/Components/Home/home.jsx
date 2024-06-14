cb0bd43dddde5496c14fc874eb0000b3b7f20af9
temidayobelloh <temidayobelloh@gmail.com>
2024-06-14 20:23:17 +0100

    worked on the home page component

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     <div>
     <div className='homePage-wrapper'>
     <div className='homePage-text'>
-    <p className='homePage-text-one'>Hello there! I am <span className='text-name'>Bello TEMIDAYO </span></p>
+    <p className='homePage-text-one'>Hello there! I'm <span className='text-name'>Bello TEMIDAYO </span></p>
     <p className='homePage-text-two'>I am Frontend Developer, who loves turning ideas into reality through code.<br></br>I specialize in creating visually appealing and functional websites using HTML, CSS, JavaScript, and React.  <br></br> I am actively searching for entry-level positions to utilize my skills and advance in my profession</p>
     <div className='button-wrapper'>
     <DownloadcvButton/>

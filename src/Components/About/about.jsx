0501abb7e247abbea92ac8668f049b0ec6959d6e
temidayobelloh <temidayobelloh@gmail.com>
2024-06-28 10:52:12 +0100

    worked on the services component

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className='about-me-wrapper'>
     <div>
     <h1 className='about-me-text'>About Me</h1>
-    <p className='about-me-intro'>I am an enthusiastic front-end developer who enjoys turning ideas into interactive websites with codes. I bring a unique combination of creativity and technical expertise, allowing me to design user experiences that are both visually stunning and functional. </p> <p className='indented'>My attention to detail ensures high-quality, polished results, while my problem-solving skills enable me to tackle challenges efficiently. I'm eagerly seeking an internship where I can contribute innovative ideas, collaborate with talented teams, and continue to grow as a developer. </p> <p className='indented'><span className='indented-color'>Let's connect and create something wonderful!</span></p>
+    <p className='about-me-intro'>Passionate about front-end development, I combine creativity and technical skills to create impactful visual experiences. <span> I build Intuitive, Responsive, & Functional user interfaces.</span> </p> <p className='indented'>My attention to detail ensures high-quality results, and my problem-solving skills enable me to handle challenges efficiently. Continously seeking the latest trends,<span> I enjoy taking on new challenges</span> and collaborating to bring ideas to life through high-quality web experiences. </p> <p className='indented'>Let's connect and create something wonderful!</p>
     </div>
     <img  className='about-me-logo' src ={aboutLogo} alt='about-logo'/>
     </div>
--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className='homePage-wrapper'>
     <div className='homePage-text'>
     <p className='homePage-text-one'>Hello there! I'm <span className='text-name'>Bello TEMIDAYO </span></p>
-    <p className='homePage-text-two'>I am Frontend Developer, who loves turning ideas into reality through code.<br></br>I specialize in creating visually appealing and functional websites using HTML, CSS, JavaScript, and React.  <br></br> I am actively searching for entry-level positions to utilize my skills and advance in my profession</p>
+    <p className='homePage-text-two'> A Frontend Developer, who loves turning ideas into reality through code.<br></br> I am actively seeking entry-level positions to utilize my skills and advance in my profession.</p>
     <div className='button-wrapper'>
     <DownloadcvButton/>
     <button className='homePage-button'>View Projects</button>

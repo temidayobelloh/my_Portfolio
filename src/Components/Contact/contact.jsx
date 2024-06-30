6ad2f836dbdec725e58349cf38ee96f88dbf70db
temidayobelloh <temidayobelloh@gmail.com>
2024-06-30 14:22:11 +0100

    worked on the footer component

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -7,11 +7,14 @@ const Contact = () => {
     <p className='contact-intro'> I am always eager to collaborate on exciting projects and bringing fresh ideas to the table. <p className='break'>If you have a project in mind, or want to discuss potential opportunities, I would love to hear from you!.</p> <p className='break'>Feel free to reach out through the form below or connect with me directly via email or social media. I look forward to exploring how we can work together to build great stuff.</p></p></div>
     <div className='contact-container'>
     <form className='form-content'> 
-    <label for='name'>Name<input type='text' placeholder='Name'/></label>
-    <label for='email'>Email<input type='email' placeholder='Email'/></label>
-    <label for='subject'>Subject<input type='text' placeholder='Subject'/></label>
-    <label for='message'>Message<textarea rows="4" cols="50"> Enter your text here...</textarea></label>
-    <button id='contactBtn'>Submit</button>
+    <label for='name'>Name</label>
+    <input type='text' id="name" name="name" placeholder='Name'/>
+    <label for='email'>Email</label>
+    <input type='email' id="email" name="email" placeholder='Email'/>
+    <label for='subject'>Subject</label>
+    <input type='text' id="subject" name="subject" placeholder='Subject'/>
+    <label for='message'>Message</label><textarea id="textarea" name="textarea" placeholder="Enter your text here..." rows="4" cols="50"></textarea>
+    <button id='contact-button'>Submit</button>
     </form>
     </div>
     </div>
--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,5 +1,8 @@
 import './footer.css';
-
+import email from '../assets/email-logo.svg';
+import location from '../assets/location-logo.svg';
+import linkedin from '../assets/icons8-linkedin.svg';
+import github from '../assets/icons8-github.svg';
 
 const handleButton = () => {
   window.scrollTo({
@@ -11,10 +14,26 @@ const handleButton = () => {
 const Footer = () => {
   return (
     <div className='footer-wrapper'>
-    <h4 className='copyright-text'>Copyright © 2024 </h4>
-    <button className='exit-Btn' onClick= {handleButton} > Back to top </button>
+      <div className='footer-content'>
+        <h3 className='contact-me-header'>Contact Me</h3>
+        <p className='contact-item'>
+          <img className="logo" src={email} alt= "email-icon"/> <a href="mailto:temidayobelloh@gmail.com">temidayobelloh@gmail.com</a>
+        </p>
+        <p className='contact-item'>
+          <img className="logo" src={location} alt="location"/> Lagos, Nigeria
+        </p>
+        <p className='contact-item'>
+          <img className="logo" src={linkedin} alt="linkedin"/> <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"> Bello Temidayo Hajarah</a>
+        </p>
+        <p className='contact-item'>
+        <img className="logo" src={github} alt="github"/><a href="https://github.com/temidayobelloh" target="_blank" rel="noopener noreferrer">github.com/temidayobelloh</a>
+        </p>
+      </div>
+      <hr></hr>
+      <h4 className='copyright-text'>Copyright © 2024</h4>
+      <button className='exit-Btn' onClick={handleButton}>Back to top</button>
     </div>
-  )
+  );
 }
 
 export default Footer;

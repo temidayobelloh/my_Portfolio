572fcc0207d90170a616ff02cf6bcc55bb615fb9
temidayobelloh <temidayobelloh@gmail.com>
2024-07-01 16:25:45 +0100

    worked on the responsiveness of the components

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     <div className='homePage-wrapper'>
     <div className='homePage-text'>
     <p className='homePage-text-one'>Hello there! I'm <span className='text-name'>Bello TEMIDAYO </span></p>
-    <p className='homePage-text-two'> A Frontend Developer, who loves turning ideas into reality through code.<br></br> I am actively seeking entry-level positions to utilize my skills and advance in my profession.</p>
+    <p className='homePage-text-two'> A Frontend Developer, who loves turning ideas into reality through code.<br></br> I am actively seeking entry-level positions to utilize my skills and advance professionally.</p>
     <div className='button-wrapper'>
     <DownloadcvButton/>
     <button onClick ={viewProject} className='viewProject-button'>View Projects </button>
